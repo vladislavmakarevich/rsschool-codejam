debcf0c8f2a9e3639ea23973209e1628385cdaf0
vladislavmakarevich <bgmakar79@gmail.com>
2018-10-28 18:55:50 +0300

    feat: add sumOfOther

---
+++ sumOfOther.js
@@ -0,0 +1,12 @@
+const sumOfOther = (arr) => {
+    let sum = 0;
+    const result = [];
+    let len = arr.length;
+    for(let i = 0; i < len; i++) {
+        sum += arr[i];
+    }
+    for(let i =0; i < len; i++) {
+        result[i] = sum - arr[i];
+    }
+    return result;
+}
